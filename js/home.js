2d0b62239c8fe1f3b69299d2742ee295f969f8e2
Tamyres <tamy.yu@hotmail.com>
2019-02-16 23:00:25 -0300

    responsividade

---
+++ js/home.js
@@ -0,0 +1,16 @@
+function fechaSlideMenu(){
+    document.getElementById('sidemenu').style.width='0';
+    document.getElementById('container').style.marginLeft='0';
+}
+function abreSlideMenu(){
+    document.getElementById('sidemenu').style.width='270px';
+    document.getElementById('container').style.marginLeft='270px';
+}
+
+function abreModal(){
+    document.getElementById('modal').style.display='block';
+}
+
+function fechaModal(){
+    document.getElementById('modal').style.display='none';
+}
